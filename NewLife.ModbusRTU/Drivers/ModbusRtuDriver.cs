9fdd03c9811332289c38da1aa7ef176c3d6c92a1
大石头 <nnhy@vip.qq.com>
2022-03-14 09:51:55 +0800

    Modbus实现NewLife.IoT协议规范，支持TSL物模型点位采集

---
+++ NewLife.ModbusRTU/Drivers/ModbusRtuDriver.cs
@@ -0,0 +1,38 @@
+﻿using NewLife.IoT.Drivers;
+using NewLife.IoT.Protocols;
+
+namespace NewLife.Serial.Protocols;
+
+/// <summary>
+/// ModbusRtu协议封装
+/// </summary>
+[Driver("ModbusRtu")]
+public class ModbusRtuDriver : ModbusDriver, IDriver
+{
+    #region 方法
+    /// <summary>
+    /// 创建Modbus通道
+    /// </summary>
+    /// <param name="channel"></param>
+    /// <param name="parameters"></param>
+    /// <returns></returns>
+    protected override Modbus CreateModbus(IChannel channel, IDictionary<String, Object> parameters)
+    {
+        var portName = parameters["PortName"] as String ?? parameters["Address"] as String;
+        if (portName.IsNullOrEmpty()) throw new ArgumentException("参数中未指定地址address");
+
+        var baudrate = parameters["Baudrate"].ToInt();
+        if (baudrate <= 0) baudrate = 9600;
+
+        var modbus = new ModbusRtu
+        {
+            PortName = portName,
+            Baudrate = baudrate,
+            Tracer = Tracer,
+            Log = Log,
+        };
+
+        return modbus;
+    }
+    #endregion
+}
