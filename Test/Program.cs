9bdd33168fa2336c069ddf440dfd3fe38523196d
大石头 <nnhy@vip.qq.com>
2022-03-14 08:00:09 +0800

    新建Modbus协议库，内置ModbusTcp

---
+++ Test/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using NewLife;
+using NewLife.Caching;
+using NewLife.Log;
+using NewLife.Security;
+using NewLife.Serialization;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            XTrace.UseConsole();
+
+            try
+            {
+                //Test1();
+            }
+            catch (Exception ex)
+            {
+                XTrace.WriteException(ex);
+            }
+
+            Console.WriteLine("OK!");
+            Console.ReadKey();
+        }
+    }
+}
